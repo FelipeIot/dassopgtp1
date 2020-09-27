2ed09b81c88b34d6fe735568115a748bb399a178
felipe sarche <sarche_2004@hotmail.com>
2020-09-27 00:35:41 -0500

    ya funciona falta editarlo

--- ParserService.py
+++ ParserService.py
@@ -1,6 +1,7 @@
 
 
-
+#!/usr/bin/python3
+import signal 
 from threading import Thread
 from time import sleep
 import socket
@@ -9,7 +10,16 @@ import json
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 8080
-MESSAGE = b"Hello, World!"
+TIEMPOLECTURA=30
+
+procesoprincipal=True
+
+def handler(sig, frame):  # define the handler  
+    print("Signal Number:", sig, " Frame: ", frame) 
+    global procesoprincipal
+    procesoprincipal=False 
+
+   
 
 class Divisas:
     def __init__(self,dir):
@@ -34,45 +44,37 @@ class Divisas:
     def get_json(self):
         lista=self.get_lista()
         item=[]
-        for i in range(len(lista)):
-            if i==0:
-                llave=lista[0].split(',')
-            else:
-                dic=dict()
-                r=lista[i].split(',')
-                dic[llave[0]]=int(r[0])
-                dic["value1"]=float(r[2])
-                dic["value2"]=float(r[3])
-                dic["name"]=r[1]
-                item.append(dic)
-        return json.dumps(item)
+        try:
+            for i in range(len(lista)):
+                if i==0:
+                    llave=lista[0].split(',')
+                else:
+                    dic=dict()
+                    r=lista[i].split(',')
+                    dic[llave[0]]=int(r[0])
+                    dic["value1"]=float(r[2])
+                    dic["value2"]=float(r[3])
+                    dic["name"]=r[1]
+                    item.append(dic)
+            return json.dumps(item)
+        except:
+            print("Error jason process")
+            return " "
         
        
         
 
 
 
-    
-        
 
 
-
-
-class hiloPrueba(Thread):
-    def __init__(self,nomb):
-        super().__init__() 
-        self.nombre=nomb
-    def run(self):
-        print('hola mundo')
-        sleep(0.5)
-
-#print(li)
-
+signal.signal(signal.SIGINT, handler) 
 ob=Divisas("config.txt")
-while True:
-    a=ob.get_json()
-    print(a)
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-    sock.sendto(bytes(a, "utf-8"), (UDP_IP, UDP_PORT))
-    sleep(30)
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
+while procesoprincipal==True:
+    sock.sendto(bytes(ob.get_json(), "utf-8"), (UDP_IP, UDP_PORT))
+    sleep(TIEMPOLECTURA)
+    print(str(procesoprincipal))
+sock.close()#cierro el puerto
+print("Cerrando")
     
